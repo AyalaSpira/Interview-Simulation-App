494db4df46f13711ba5a532b01ca187a2952f99d
Your Name <youremail@example.com>
2025-04-22 18:26:21 +0300

    הוספתי שרותים למנהל וריפו

--- backend/InterviewSimulationApp/InterviewSim.DAL/Entities/Admin.cs
+++ backend/InterviewSimulationApp/InterviewSim.DAL/Entities/Admin.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InterviewSim.DAL.Entities
 {
+    [Table("admins")]
     public class Admin
     {
         public int Id { get; set; }
